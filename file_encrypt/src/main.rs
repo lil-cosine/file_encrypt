8e4e5d7f4a4ed0b0d5f6eb1f31c9659b2ad1121a
lil_cosine <baeden.vester@gmail.com>
2025-01-23 23:16:52 -0500

    add recursive encryption/decryption for directories and multiple files

--- file_encrypt/src/main.rs
+++ file_encrypt/src/main.rs
@@ -14,26 +14,45 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if &args[1] == "generate" {
         generate_key();
+    } else if args.iter().any(|arg| arg == "-r") {
+        let cipher = retrive_cipher();
+        if args.iter().any(|arg| arg == "-e") || args.iter().any(|arg| arg == "--encrypt") {
+            let path = Path::new(&args[3]);
+            if path.is_dir() {
+                for entry in fs::read_dir(&args[3]).expect("Unable to read directory") {
+                    let entry = entry.expect("Unable to read entry");
+                    let path = entry.path();
+                    let path_str = path.to_str().unwrap();
+                    encrypt(cipher.clone(), path_str);
+                }
+            } else {
+                println!("Invalid usage. Must provide a directory to encrypt.");
+            }
+        } else if args.iter().any(|arg| arg == "-d") || args.iter().any(|arg| arg == "--decrypt") {
+            let path = Path::new(&args[3]);
+            if path.is_dir() {
+                for entry in fs::read_dir(&args[3]).expect("Unable to read directory") {
+                    let entry = entry.expect("Unable to read entry");
+                    let path = entry.path();
+                    let path_str = path.to_str().unwrap();
+                    decrypt(cipher.clone(), path_str);
+                }
+            } else {
+                println!("Invalid usage. Must provide a directory to decrypt.");
+            }
+        }
     } else if &args[1] == "--encrypt" || &args[1] == "-e" {
-        let vals = retrive_key();
-        let key_bytes = base64::decode(&vals.0).expect("Invalid base64 key");
-        let nonce_bytes = base64::decode(&vals.1).expect("Invalid base64 nonce");
-        let key = GenericArray::from_slice(&key_bytes);
-        let nonce = GenericArray::from_slice(&nonce_bytes);
-        let cipher = ctr::Ctr128BE::<Aes128>::new(&key, &nonce);
-        encrypt(cipher, &args[2]);
+        let cipher = retrive_cipher();
+        for i in 2..args.len() {
+            encrypt(cipher.clone(), &args[i]);
+        }
     } else if &args[1] == "--decrypt" || &args[1] == "-d" {
-        let vals = retrive_key();
-        let key_bytes = base64::decode(&vals.0).expect("Invalid base64 key");
-        let nonce_bytes = base64::decode(&vals.1).expect("Invalid base64 nonce");
-        let key = GenericArray::from_slice(&key_bytes);
-        let nonce = GenericArray::from_slice(&nonce_bytes);
-        let cipher = ctr::Ctr128BE::<Aes128>::new(&key, &nonce);
-        decrypt(cipher, &args[2]);
+        let cipher = retrive_cipher();
+        for i in 2..args.len() {
+            decrypt(cipher.clone(), &args[i]);
+        }
     } else if &args[1] == "--help" || &args[1] == "-h" {
         help();
-    } else if &args[1] == "-r" {
-        println!("This is a test");
     } else {
         println!("Invalid command. Use 'help' or '-h' for help.");
     }
@@ -50,7 +69,7 @@ fn generate_key() {
     fs::write("src/nonce.txt", encode(&nonce)).expect("Unable to write nonce");
 }
 
-fn retrive_key() -> (String, String) {
+fn retrive_cipher() -> ctr::Ctr128BE<Aes128> {
     let mut key_file = File::open("src/key.txt").expect("File not found");
     let mut key = String::new();
     key_file
@@ -61,7 +80,12 @@ fn retrive_key() -> (String, String) {
     nonce_file
         .read_to_string(&mut nonce)
         .expect("Could not read file");
-    (key, nonce)
+    let key_bytes = base64::decode(key).expect("Invalid base64 key");
+    let nonce_bytes = base64::decode(nonce).expect("Invalid base64 nonce");
+    let key = GenericArray::from_slice(&key_bytes);
+    let nonce = GenericArray::from_slice(&nonce_bytes);
+    let cipher = ctr::Ctr128BE::<Aes128>::new(&key, &nonce);
+    cipher
 }
 
 fn help() {
